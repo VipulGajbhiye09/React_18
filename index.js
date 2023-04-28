be076db440669c00cffbcc2a49805e747ccc8949
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-04-28 11:55:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ function Message() {
   return <h1>My First Component</h1>;
 }
 
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/91qXDU9TXvL._AC_UL600_SR600,400_.jpg"
+    alt="The Wager: A Tale of Shipwreck, Mutiny and Murder"
+  />
+);
+const Title = () => {
+  return <h2>The Wager: A Tale of Shipwreck, Mutiny and Murder</h2>;
+};
+const Author = () => <h4>David Grann</h4>;
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<Message />);
