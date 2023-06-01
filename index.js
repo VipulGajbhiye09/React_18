6057f22888bfdf9c32fd9fd35832017a7e2d61fb
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-06-01 18:44:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css'
 
+const author = "David Grann";
+const title = "The Wager: A Tale of Shipwreck, Mutiny and Murder";
+const img ="https://images-na.ssl-images-amazon.com/images/I/91qXDU9TXvL._AC_UL600_SR600,400_.jpg";
+
 function BookList() {
   return (
     <section className="booklist">
@@ -23,16 +27,6 @@ const Book = () => {
   );
 };
 
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/91qXDU9TXvL._AC_UL600_SR600,400_.jpg"
-    alt="The Wager: A Tale of Shipwreck, Mutiny and Murder"
-  />
-);
-const Title = () => {
-  return <h2>The Wager: A Tale of Shipwreck, Mutiny and Murder</h2>;
-};
-const Author = () => <h4>David Grann</h4>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
