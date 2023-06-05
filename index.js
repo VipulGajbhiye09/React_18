6f68c5e9e730a27a1f1f00c7f62431be22c23709
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-06-05 23:56:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,15 +42,16 @@ const Book = (props) => {
   );
 };
 //access props - multiple approaches
-// const someObject = {
-//   name: 'john',
-//   job: 'developer',
-//   location: 'florida',
+// const Book = (props) => {
+//   const { img, title, author } = props;
+//   return (
+//     <article className='book'>
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h4>{author} </h4>
+//     </article>
+//   );
 // };
-
-// console.log(someObject.name);
-// const { name, job } = someObject;
-// console.log(job);
 //
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
