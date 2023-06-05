4dadfd1a127955888a97e79888571b7668687b18
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-06-05 23:57:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,19 @@ const Book = (props) => {
 //     </article>
 //   );
 // };
-//
+
+//- destructure in function parameters (in our case props)
+
+// const Book = ({ img, title, author }) => {
+//   return (
+//     <article className='book'>
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h4>{author} </h4>
+//     </article>
+//   );
+// };
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
