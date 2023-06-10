2f4eada01aeac3706ffda06daa0d6949bfaa754c
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-06-10 23:56:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const Book = (props) => {
 //one of preffered ways for using react props
 
 //- destructure in function parameters (in our case props)
-//- if you have console.log(props) - it won't be defined
+//- if you have console.log(props) - it won't be defined so use accordingly
 // const Book = ({ img, title, author }) => {
 //   return (
 //     <article className='book'>
