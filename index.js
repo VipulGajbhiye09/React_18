1f716d0df5ca2cf46aad0ac4c15a6163820dab2e
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-07-08 23:18:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,11 +23,7 @@ function BookList() {
         title={firstBook.title}
         img={firstBook.img}
       >
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
-          repudiandae inventore eos qui animi sed iusto alias eius ea sapiente.
-        </p>
-        <button>click me</button>
+
       </Book>
             
       <Book
