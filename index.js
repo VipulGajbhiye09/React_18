57675c52b33134a62c4d9a0ace18ee85e0622ee2
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-07-26 23:10:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,21 +44,21 @@ const Book = (props) => {
 };
 
 
+//getBook function
+const BookList = () => {
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
 
-//one of the preferred ways for using react props
-
-//- destructure in function parameters (in our case props)
-//- if you have console.log(props) - it won't be defined so use accordingly
-
-// const Book = ({ img, title, author }) => {
-//   return (
-//     <article className='book'>
-//       <img src={img} alt={title} />
-//       <h2>{title}</h2>
-//       <h4>{author} </h4>
-//     </article>
-//   );
-// };
+  return (
+    <section className='booklist'>
+      {books.map((book) => {
+        return <Book {...book} key={book.id} getBook={getBook} />;
+      })}
+    </section>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
