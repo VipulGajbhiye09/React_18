0f1cf4b4ff42982518b634c03fe757f50223ea38
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-08-06 23:06:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,80 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css'
+import "./index.css";
 
-const books = [
-  {
-    author: "Jordan Moore",
-    title: "Interesting Facts For Curious Minds",
-    img: "https://images-na.ssl-images-amazon.com/images/I/91qXDU9TXvL._AC_UL600_SR600,400_.jpg",
-    id: 1,
-  },
-  {
-    author: "James Clear",
-    title: "Atomic Habits",
-    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
-    id: 2,
-  },
-];
+import {books} from './books'
 
-//refactor using map functions and key prop
+import Book from "./Book";
+
 function BookList() {
   return (
-    <section className="booklist">
-      {books.map((book) => {
-        console.log(book);
-        const { img, title, author, id } = book;
-        return <Book img={img} title={title} author={author} key={id} />;
-      })}
-    </section>
+    <>
+      <h1>amazon best sellers</h1>
+      <section className="booklist">
+        {books.map((book,index) => {
+          return <Book {...book} key={book.id} number={index}/>;
+        })}
+      </section>
+    </>
   );
 }
 
 
-const Book = (props) => {
-  const { img, title, author } = props;
-  
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author} </h4>
-    </article>
-  );
-};
-
-
-//getBook function
-const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id);
-    console.log(book);
-  };
-
-  return (
-    <section className='booklist'>
-      {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />;
-      })}
-    </section>
-  );
-};
-const Book = (props) => {
-  const { img, title, author, getBook, id } = props;
-  // console.log(props);
-
-  return (
-    <article className='book'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      {/* this is not going to work */}
-      <button onClick={getBook(id)}>display title</button>
-      <h4>{author}</h4>
-    </article>
-  );
-};
-//above wont work as intended
-//
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
